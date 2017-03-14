3b62444b23baf8be99438855257cfe943ff99ea2
vykern <vykern@163.com>
2017-03-14 16:47:53 +0800

    创建项目

---
+++ src/hello_git/Hello_Git.java
@@ -0,0 +1,18 @@
+package hello_git;
+
+public class Hello_Git {
+
+	public Hello_Git() {
+		// TODO 自动生成的构造函数存根
+	}
+
+	public static void main(String[] args) {
+		Hello_Git hg = new Hello_Git();
+		hg.print("Hello Git!");
+	}
+
+	private void print(String string) {
+		System.out.println(string);
+	}
+
+}
